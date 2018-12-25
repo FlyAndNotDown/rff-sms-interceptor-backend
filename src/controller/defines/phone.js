3246936b84e610b875f05076427a1add8e10aefc
John Kindem <461425614@qq.com>
2018-12-25 17:49:34 +0800

    [add] add ad RESTful API

--- src/controller/defines/phone.js
+++ src/controller/defines/phone.js
@@ -108,5 +108,61 @@ export default {
                     return null;
             }
         }
+    },
+    put: (db, models) => {
+        return async (ctx, next) => {
+            await next();
+
+            // 获取参数
+            const requestBody = ctx.request.body || {};
+            const number = requestBody.number || null;
+
+            // 参数校验
+            if (!number || !number.match(normalRegex.naturalNumber)) {
+                Log.error('status 400', `number: ${number}`);
+                ctx.response.status = 400;
+                return null;
+            }
+
+            // 参数转换
+            number = parseInt(number);
+
+            // 查询数据库
+            let phone;
+            try {
+                phone = models.phone.findOne({
+                    where: {
+                        number: {
+                            [SequelizeOp.eq]: number
+                        }
+                    }
+                });
+            } catch (e) {
+                Log.error('status 500', e);
+                ctx.response.status = 500;
+                return null;
+            }
+
+            // 如果没有查询到数据
+            if (!phone) {
+                return ctx.response.body = {
+                    success: false
+                };
+            }
+
+            // 自增举报数量
+            try {
+                await phone.increment('blockNum');
+            } catch(e) {
+                Log.error('status 500', e);
+                ctx.response.status = 500;
+                return null;
+            }
+
+            // 返回结果
+            return ctx.response.body = {
+                success: true
+            };
+        }
     }
 }
